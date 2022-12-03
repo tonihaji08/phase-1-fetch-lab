ed96288cb8cb04b945039e9c7cc79d0a1ee3579b
Your Name <antony.adriano@stundent.moringaschool.com>
2022-12-03 20:14:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch('https://anapioficeandfire.com/api/books')
+   .then (function(response) {
+      return response.json();
+   })
+    .then (function(json) {
+      renderBooks(json);
+    });
   
 }
 
@@ -15,3 +22,4 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
